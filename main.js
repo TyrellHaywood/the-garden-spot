24ee0648fcc2fb3544635ff076df3ac5b91b1a4d
TyrellHaywood <tyrelltman@gmail.com>
2024-02-06 12:24:41 -0500

    signup form structure

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // nav bar elements
     const toggleMenuBtn = document.getElementById('toggle-menu');
     const menuItems = document.getElementById('menu-items');
     const header = document.getElementById('header');
     const nav = document.getElementById('nav');
     const infoContainer = document.getElementById('info-container');
+  
+    // sign up elements
 
+    // nav bar event lsitener
     toggleMenuBtn.addEventListener('click', () => {
       menuItems.classList.toggle('hidden');
       header.classList.toggle('hidden');
