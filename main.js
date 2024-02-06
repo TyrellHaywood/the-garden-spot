053e88b57271b3a1b95d41d8a9a99eaa6de19069
TyrellHaywood <tyrelltman@gmail.com>
2024-02-06 12:34:46 -0500

    added signup form button

--- main.js
+++ main.js
@@ -8,13 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const infoContainer = document.getElementById('info-container');
   
     // sign up elements
+    const signUpForm = document.getElementById('sign-up-form');
+    const signUpButton = document.getElementById('sign-up');
 
-    // nav bar event lsitener
+    // nav bar event lsitener fxn
     toggleMenuBtn.addEventListener('click', () => {
       menuItems.classList.toggle('hidden');
       header.classList.toggle('hidden');
       nav.classList.toggle('hidden');
 
       infoContainer.classList.toggle('menu-opened');
+    });
+
+
+    signUpButton.addEventListener('click', () => {
+
+      signUpForm.classList.toggle('opened');
     });
   });
