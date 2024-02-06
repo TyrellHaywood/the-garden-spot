eb6ac5cf298984675d0b459fa6c0d9405adf3a13
TyrellHaywood <tyrelltman@gmail.com>
2024-02-05 21:03:16 -0500

    dynamic navbar

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleMenuBtn = document.getElementById('toggle-menu');
+    const menuItems = document.getElementById('menu-items');
+    const header = document.getElementById('header');
+    const nav = document.getElementById('nav');
+    const infoContainer = document.getElementById('info-container');
 
+    toggleMenuBtn.addEventListener('click', () => {
+      menuItems.classList.toggle('hidden');
+      header.classList.toggle('hidden');
+      nav.classList.toggle('hidden');
+
+      infoContainer.classList.toggle('menu-opened');
+    });
+  });
